bbd012db0c75cc9f5ae091e55ed9ffc9b7514c10
Haldian rnw <haldian23@gmail.com>
2025-06-25 10:55:59 +0700

    feat: add search dataset functionality with controller, views, and routing

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <i class="fas fa-database me-2"></i>
+      <span class="brand-text">Portal Data Tulang Bawang</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="#home">Beranda</a></li>
+        <li class="nav-item"><a class="nav-link" href="#data">Data Terkini</a></li>
+        <li class="nav-item"><a class="nav-link" href="#data-section">Kategori Data</a></li>
+        <li class="nav-item"><a class="nav-link" href="#layanan">Layanan</a></li>
+        <li class="nav-item"><a class="nav-link" href="#kontak">Kontak</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/search-dataset', [App\Http\Controllers\SearchDatasetController::class, 'index'])
+    ->name('search-dataset.index');
